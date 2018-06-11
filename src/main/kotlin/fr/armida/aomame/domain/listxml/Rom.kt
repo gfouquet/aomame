43faf51764871880d889c79c49715cedc7789889
Grégory Fouquet <gfouquet@armida.fr>
2018-06-11 22:23:42 +0200

    Listxml: Retrofitted Rom entity with Status enum

--- src/main/kotlin/fr/armida/aomame/domain/listxml/Rom.kt
+++ src/main/kotlin/fr/armida/aomame/domain/listxml/Rom.kt
@@ -1,9 +1,10 @@
 package fr.armida.aomame.domain.listxml
 
+import fr.armida.aomame.domain.listxml.Status.GOOD
+import fr.armida.aomame.xml.StatusAdapter
 import fr.armida.aomame.xml.YesNoAdapter
 import javax.persistence.*
 import javax.xml.bind.annotation.*
-import javax.xml.bind.annotation.adapters.CollapsedStringAdapter
 import javax.xml.bind.annotation.adapters.NormalizedStringAdapter
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter
 
@@ -13,7 +14,7 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter
 @XmlRootElement(name = "rom")
 @Entity
 // TODO migrate to kotlin
-data class Rom (
+data class Rom(
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_rom")
     @SequenceGenerator(name = "seq_rom", sequenceName = "seq_rom", allocationSize = 1000)
@@ -21,52 +22,37 @@ data class Rom (
     // TODO hibernate id
     val id: Long? = null,
 
-
-
     @XmlAttribute(name = "name", required = true)
     @XmlJavaTypeAdapter(NormalizedStringAdapter::class)
     val name: String,
-
-
 
     @XmlAttribute(name = "bios")
     @XmlJavaTypeAdapter(NormalizedStringAdapter::class)
     val bios: String,
 
-
-
     @XmlAttribute(name = "size", required = true)
     @Transient
     var size: Long,
-
-
 
     @XmlAttribute(name = "crc")
     @XmlJavaTypeAdapter(NormalizedStringAdapter::class)
     @Transient
     val crc: String,
 
-
-
     @XmlAttribute(name = "sha1")
     @XmlJavaTypeAdapter(NormalizedStringAdapter::class)
     @Transient
     val sha1: String,
-
 
     @XmlAttribute(name = "merge")
     @XmlJavaTypeAdapter(NormalizedStringAdapter::class)
     @Transient
     val merge: String,
 
-
-
     @XmlAttribute(name = "region")
     @XmlJavaTypeAdapter(NormalizedStringAdapter::class)
     @Transient
     val region: String,
-
-
 
     @XmlAttribute(name = "offset")
     @XmlJavaTypeAdapter(NormalizedStringAdapter::class)
@@ -74,12 +60,12 @@ data class Rom (
     val offset: String,
 
     @XmlAttribute(name = "status")
-    @XmlJavaTypeAdapter(CollapsedStringAdapter::class)
-    val status: String? = "good",
+    @XmlJavaTypeAdapter(StatusAdapter::class)
+    val status: Status,
 
     @XmlAttribute(name = "optional")
     @XmlJavaTypeAdapter(YesNoAdapter::class)
-    var optional: Boolean
+    val optional: Boolean
 ) {
-    constructor(): this(null, "", "", 0, "", "", "", "", "", "", false)
+    constructor() : this(null, "", "", 0, "", "", "", "", "", GOOD, false)
 }
--- src/test/kotlin/fr/armida/aomame/domain/listxml/RomTest.kt
+++ src/test/kotlin/fr/armida/aomame/domain/listxml/RomTest.kt
@@ -1,6 +1,7 @@
 package fr.armida.aomame.domain.listxml
 
 import com.winterbe.expekt.should
+import fr.armida.aomame.domain.listxml.Status.*
 
 internal class RomTest : JaxbBindingTest<Rom>() {
     init {
@@ -13,6 +14,7 @@ internal class RomTest : JaxbBindingTest<Rom>() {
             res.sha1.should.equal("c4b4e6c75bcf276e53f39a456d8d633c83dcf485")
             res.region.should.equal("maincpu")
             res.offset.should.equal("800")
+            res.status.should.equal(GOOD)
         }
     }
 }
@@ -24,7 +26,7 @@ internal class RomNoDumpTest : JaxbBindingTest<Rom>() {
         then {
             res.name.should.equal("bm.7k")
             res.size.should.equal(65536)
-            res.status.should.equal("nodump")
+            res.status.should.equal(NODUMP)
             res.region.should.equal("mcu")
             res.offset.should.equal("0")
         }
