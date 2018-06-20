31160c46339d34c91f919143d6625413e10b357f
Grégory Fouquet <gfouquet@armida.fr>
2018-06-20 21:49:10 +0200

    Spring: configured jaxb marshaller

--- src/main/kotlin/fr/armida/aomame/AmoameApplication.kt
+++ src/main/kotlin/fr/armida/aomame/AmoameApplication.kt
@@ -1,10 +1,22 @@
 package fr.armida.aomame
 
+import fr.armida.aomame.domain.listxml.Mame
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.oxm.jaxb.Jaxb2Marshaller
 
 @SpringBootApplication
-class AmoameApplication
+class AmoameApplication {
+    @Bean
+    fun jaxb2Marshaller(): Jaxb2Marshaller {
+        val bean = Jaxb2Marshaller()
+        bean.setClassesToBeBound(Mame::class.java)
+        bean.isSupportDtd = true
+
+        return bean
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<AmoameApplication>(*args)
